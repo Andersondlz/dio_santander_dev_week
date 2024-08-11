0b9e9cddea6b95800871bf962292fcfd14573097
Andersondlz <andersondlz2009@gmail.com>
2024-08-10 21:35:46 -0300

    commit

--- src/test/java/me/dio/SantaderDevWeek2004ApplicationTests.java
+++ src/test/java/me/dio/SantaderDevWeek2004ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SantaderDevWeek2004ApplicationTests {
+class SantanderDevWeek2023ApplicationTests {
 
 	@Test
 	void contextLoads() {
